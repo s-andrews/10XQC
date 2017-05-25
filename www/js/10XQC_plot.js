a13357644d5ace4e0184a4eaadc8931f68777b87
Phil Ewels <phil.ewels@scilifelab.se>
2017-05-25 12:05:39 +0200

    Added error handling for UMI plot. Properly fetch sample names.

--- www/js/10XQC_plot.js
+++ www/js/10XQC_plot.js
@@ -58,77 +58,93 @@ $(function() {
 
 function plot_umi_barcodes(){
 
-  // Get the IDs of the reports to plot
-  samples = [];
-  $('#sample_browse_table tbody tr').each(function(){
-    samples.push( $(this).attr('id').replace('row_', '') );
-  });
+  try {
 
-  // Find which column is sorted
-  var sorted_col_idx = $('#sample_browse_table').dataTable().fnSettings().aaSorting[0][0];
-  var sort_col = $('#table_columns_modal_table input:checked').eq(parseInt(sorted_col_idx)).attr('name');
-  $('#umi_plot_modal_cat_1 option[value="'+sort_col+'"]').attr('selected', true);
-
-  // Get the default colours
-  var colors = Plotly.d3.scale.category20();
-  // List to hold unique names
-  var names = [];
-
-  // Load the data
-  var api_url = plot_api+'ids='+samples.join(',');
-  $.getJSON(api_url, function(ajax_data) {
-    $('#umi_plot_modal_plotdiv').empty();
-    pdata = [];
-    $.each(ajax_data['data'], function(idx, data){
-      name = t_data[data[0]][sort_col];
-      if(names.indexOf(name) < 0){
-        names.push(name);
-      }
-      var colour = colors(names.indexOf(name));
-      pdata.push( {
-        name: name,
-        uid: data[0],
-        x: data[1][0],
-        y: data[1][1],
-        mode: 'lines',
-        type: 'scattergl',
-        hoverinfo: 'name',
-        line: {
-          width: 3,
-          color: colour
-        }
-      });
+    // Get the IDs of the reports to plot
+    samples = [];
+    $('#sample_browse_table tbody tr').each(function(){
+      samples.push( $(this).attr('id').replace('row_', '') );
     });
-    // Make the plot
-    var layout = {
-      title:'UMI Counts vs Barcode',
-      height: 500,
-      width: 500,
-      hovermode: 'closest',
-      xaxis: {
-        title: 'Barcodes',
-        type: 'log'
-      },
-      yaxis: {
-        title: 'UMI Counts',
-        type: 'log'
+
+    // Find which column is sorted
+    var sorted_col_idx = $('#sample_browse_table').dataTable().fnSettings().aaSorting[0][0];
+    var sort_col = $('#table_columns_modal_table input:checked').eq(parseInt(sorted_col_idx)).attr('name');
+    $('#umi_plot_modal_cat_1 option[value="'+sort_col+'"]').attr('selected', true);
+
+    // Get the default colours
+    var colors = Plotly.d3.scale.category20();
+    // List to hold unique names
+    var names = [];
+
+    // Load the data
+    var api_url = plot_api+'ids='+samples.join(',');
+    $.getJSON(api_url, function(ajax_data) {
+      try {
+        $('#umi_plot_modal_plotdiv').empty();
+        pdata = [];
+        $.each(ajax_data['data'], function(idx, data){
+          name = '';
+          for(i=0; i<t_data.length; i++){
+            if(t_data[i]['DT_RowId'].replace('row_','') == data[0]){
+              name = t_data[i][sort_col];
+            }
+          }
+          if(names.indexOf(name) < 0){
+            names.push(name);
+          }
+          var colour = colors(names.indexOf(name));
+          pdata.push( {
+            name: name,
+            uid: data[0],
+            x: data[1][0],
+            y: data[1][1],
+            mode: 'lines',
+            type: 'scattergl',
+            hoverinfo: 'name',
+            line: {
+              width: 3,
+              color: colour
+            }
+          });
+        });
+        // Make the plot
+        var layout = {
+          title:'UMI Counts vs Barcode',
+          height: 500,
+          width: 500,
+          hovermode: 'closest',
+          xaxis: {
+            title: 'Barcodes',
+            type: 'log'
+          },
+          yaxis: {
+            title: 'UMI Counts',
+            type: 'log'
+          }
+        };
+        var config = {
+          displaylogo: false,
+          showlink: false,
+          modeBarButtonsToRemove: [
+            // "zoom2d",
+            // "pan2d",
+            // "zoomIn2d",
+            // "zoomOut2d",
+            "autoScale2d",
+            // "toImage",
+            "sendDataToCloud"
+          ]
+        };
+        Plotly.newPlot('umi_plot_modal_plotdiv', pdata, layout, config);
+      } catch(e){
+        $('.umi_plot_modal_plotdiv_wrapper').empty().html('<div id="umi_plot_modal_plotdiv"><div class="alert alert-danger mt-5"><strong>Error:</strong> Something went wrong when making the plot.</div></div>');
+        console.error(e);
       }
-    };
-    var config = {
-      displaylogo: false,
-      showlink: false,
-      modeBarButtonsToRemove: [
-        // "zoom2d",
-        // "pan2d",
-        // "zoomIn2d",
-        // "zoomOut2d",
-        "autoScale2d",
-        // "toImage",
-        "sendDataToCloud"
-      ]
-    };
-    Plotly.newPlot('umi_plot_modal_plotdiv', pdata, layout, config);
-  });
+    });
+  } catch(e){
+    $('.umi_plot_modal_plotdiv_wrapper').empty().html('<div id="umi_plot_modal_plotdiv"><div class="alert alert-danger mt-5"><strong>Error:</strong> Something went wrong when making the plot.</div></div>');
+    console.error(e);
+  }
 }
 
 function update_umi_plot_names(){
